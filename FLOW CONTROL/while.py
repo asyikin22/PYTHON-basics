290d880379eb35e103634e11ab32746c8bde9ec1
asyikin22 <148519441+asyikin22@users.noreply.github.com>
2024-04-23 21:34:29 +0800

    conditional statements

---
+++ FLOW CONTROL/while.py
@@ -0,0 +1,17 @@
+# #if statemetn
+# spam = 0
+# if spam < 5:
+#     print('Hello World')
+
+#     spam = spam + 1
+
+
+# while statement
+spam = 0
+
+while spam < 5:
+    print('Hello World')
+
+    spam = spam + 1
+
+    
