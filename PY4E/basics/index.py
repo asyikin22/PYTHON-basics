aa539dea42db8b768eeea4330c8287f54a6dd805
asyikin22 <148519441+asyikin22@users.noreply.github.com>
2024-07-08 21:22:35 +0800

    new folder

---
+++ PY4E/basics/index.py
@@ -0,0 +1,16 @@
+# conditional steps
+x = 5
+if x < 10:
+    print("smaller")
+if x > 2:
+    print("bigger")
+
+print("Asyikin")
+
+# repeated steps
+n = 10
+while n > 0:
+    print(n)
+    n = n-1
+print("Hooray!")
+
