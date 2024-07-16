2c7d156e4dd49c4f240a80bc27453db31fa809c0
asyikin22 <148519441+asyikin22@users.noreply.github.com>
2024-07-16 10:57:06 +0800

    more loops

--- PY4E/basics/loops.py
+++ PY4E/basics/loops.py
@@ -38,5 +38,61 @@ for nombor in [99, 56, 43, 18, 72, 402]:
 print("Selepas", largest_so_far)
 
 #Counting in a loop
+kosong = 0
+print("Sebelum", kosong)
+for benda in[99, 56, 43, 18, 72, 402]:
+    kosong = kosong + 2
+    print (kosong, benda)
+print("Selepas", kosong)
 
+#summing in a loop
+dua = 2
+print("sebelum", dua)
+for nombor in [1, 2, 3, 4, 5]:
+    dua = dua + nombor
+    print(dua, nombor)
+print("selepas", dua)
 
+#finding average in a loop
+kira = 0
+jumlah = 0
+print("sebelum", kira, jumlah)
+for nilai in [3, 6, 9, 12, 15]:
+    kira = kira + 1
+    jumlah = jumlah + nilai
+    print(kira, jumlah, nilai)
+print('selepas', kira, jumlah, jumlah/kira)
+
+#filtering in a loop
+print('sebelum')
+for nilai in [99, 56, 43, 18, 72, 402]:
+    if nilai > 95:
+        print("Nombor besar:", nilai)
+print("selepas")
+
+#search using a boolean variable
+found = False
+print('sebelum', found)
+for nilai in [56, 43, 18, 72, 99, 402]:
+    if nilai == 99:
+        found = True
+    print(found, nilai)
+print('Selepas', found)
+
+#finding the largest value
+nombor_terbesar = 0
+print('sebelum', nombor_terbesar)
+for nombor in [56, 43, 18, 72, 99, 402]:
+    if nombor > nombor_terbesar:
+        nombor_terbesar = nombor
+    print(nombor_terbesar, nombor)
+print('selepas', nombor_terbesar)
+
+#finding the smallest value
+nombor_terkecil = 20
+print('sebelum', nombor_terkecil)
+for nombor in [56, 43, 18, 72, 99, 402]:
+    if nombor < nombor_terkecil:
+        nombor_terkecil = nombor
+    print(nombor_terkecil, nombor)
+print('selepas', nombor_terkecil)
