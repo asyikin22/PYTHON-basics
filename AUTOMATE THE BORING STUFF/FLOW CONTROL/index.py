2345b7f6e181a7d66db0a187dc03f4d8deaef4b6
asyikin22 <148519441+asyikin22@users.noreply.github.com>
2024-07-21 16:25:20 +0800

    conditional statement, while loop, break, continue

--- AUTOMATE THE BORING STUFF/FLOW CONTROL/index.py
+++ AUTOMATE THE BORING STUFF/FLOW CONTROL/index.py
@@ -1,12 +1,39 @@
-name = 'Asyikin'
+# conditional statements + input validation
+name = input("Insert your name: ") 
 
-password = "cats"
+password = input("Insert your password: ")
 
+#check if the name is correct
 if name == 'Asyikin':               # first block of code
     print('Hello Asyikin')
 
+    #check if the password is correct
     if password == "cats":
         print('Access Granted')     # second block of code
 
     else:
         print('Wrong Password')     # third block of codea
+
+else:
+    print('Unauthorized User')
+
+#comparison operators
+x = 99 == 99
+print(x)
+
+y = 99 == 0
+print(y)
+
+z = 1 != 2
+print(z)
+
+print("strings comparison")
+
+nama = 'asyikin' == "Asyikin"
+print(nama)
+
+nama2 = 'asyikin' != "Asyikin"
+print(nama2)
+
+
+
--- AUTOMATE THE BORING STUFF/FLOW CONTROL/statement.py
+++ AUTOMATE THE BORING STUFF/FLOW CONTROL/statement.py
@@ -1,33 +1,89 @@
 # if statement
-# name = 'Asyikin'
-# if name == 'Asyikin':
-#     print('Hi, Asyikin')
-
-# else:
-#     print('Hello Stranger')
-
-#else statement
-# name = 'Anna'
-# if name == 'Asyikin':
-#     print('Hi, Asyikin')
-
-# else:
-#     print('Hello Stranger')
-
-#elif statement
-name = 'Anna'
-age = 50
+name = "Asyikin"
 
 if name == 'Asyikin':
     print('Hi, Asyikin')
 
-elif age < 12:
-    print('You are not Asyikin, child')
+# else statement
+inpName = input("Insert a name: ")
 
-elif age > 18:
-    print('Unlike you, Anna is more mature and experienced!')
+if inpName == 'Austin':
+    print('Hi, Austin')
 
-elif age > 100:
+else:
+    print('Hello Stranger')
+
+#elif statement
+name = 'Anna'
+age = input("Insert your age here: ")
+
+if age == '35':
+    print('Hi, Lily')
+
+elif int(age) < 12:
+    print('You are not Anna, child')
+
+elif int(age) > 50:
     print('You are not Anna, Grandma')
 
+elif int(age) > 18:
+    print('Unlike you, Anna is more mature and experienced!')
 
+#elseif - else
+name = "Kucing"
+umur = input("Berapa umur awak? - ")
+
+if int(umur) == 10:
+    print("Hi, Kucing, meows 🐾")
+elif int(umur) < 10:
+    print("You are a baby, you're not Kucing!")
+else:
+    print("You're not Kucing either!")
+
+#While statement 1
+kira = 0
+while kira < 5:
+    print("Konnichiwa", (kira))
+
+    kira = kira + 1
+
+#break statement
+while True:
+    print("Please type your name.")
+    name = input()
+    if name == 'Spongebob':
+        break
+
+print('Terima kasih')
+
+#While statement 2
+counter = 0
+while counter < 10:
+    if counter == 5:
+        break
+    print(counter)
+    counter += 1
+
+#While statement 3 - For loop
+print("For loop - break statement")
+for item in range(10):
+    if item == 4:
+        break
+    print(item)
+
+#continue statement
+print("Continue statement")
+
+while True:
+    print("Siapa awak?")
+    name = input("Nama saya: ")
+
+    if name != "asyikin":
+        continue
+    print("Hi Asyikin, sila masukkan password") 
+
+    password = input()
+    if password == "abcde":
+        break
+
+print("Access granted")
