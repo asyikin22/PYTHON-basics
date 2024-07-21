d07ef72e1a80cf3021ca876473c782eea9fd46ca
asyikin22 <148519441+asyikin22@users.noreply.github.com>
2024-07-21 13:14:57 +0800

    python basics + practice qs

--- AUTOMATE THE BORING STUFF/BASIC/hello.py
+++ AUTOMATE THE BORING STUFF/BASIC/hello.py
@@ -1,33 +1,87 @@
 # This program says hello and asks for my name
 
-print('Hello World')
-print('What is your name?')     # ask for thier name
+# print('Hello World')
+# print('What is your name?')     # ask for thier name
 
-myName = input()
+# myName = input()
 
-print('It is nice to meet you, ' + myName)
+# print('It is nice to meet you, ' + myName)
 
-print('The length of your name is:')
-print(len(myName))
+# print('The length of your name is:')
+# print(len(myName))
 
-print('What is your age?')      # ask for thier age
+# print('What is your age?')      # ask for thier age
 
-myAge = input()
+# myAge = input()
 
-print('You will be ' + str(int(myAge) +1) + ' in a year.')
+# print('You will be ' + str(int(myAge) +1) + ' in a year.')
 
-#secret password
-password = open("failRahsia.txt")
-passwordRahsia = password.read().strip()
-password.close()
+# #secret password
+# password = open("failRahsia.txt")
+# passwordRahsia = password.read().strip()
+# password.close()
 
-taipPassword = input('Masukkan Password: ')
+# taipPassword = input('Masukkan Password: ')
 
-if taipPassword == 'abcde':
-    print("Password ini salah, sila cuba sekali lagi")
+# if taipPassword == 'abcde':
+#     print("Password ini salah, sila cuba sekali lagi")
     
-elif taipPassword == passwordRahsia:
-    print("Sila masuk ke website")
+# elif taipPassword == passwordRahsia:
+#     print("Sila masuk ke website")
     
-else: 
-    print("Anda tidak dibenarkan masuk!")
+# else: 
+#     print("Anda tidak dibenarkan masuk!")
+
+#math operation
+exponent = 2 ** 3
+print(exponent) # 8
+
+modulus = 37 % 5
+print(modulus) #2
+
+bahagiInteger = 38 // 6
+print(bahagiInteger)  #6
+
+bahagi = 100/20
+print(bahagi)
+
+darab = 99 * 99
+print(darab)
+
+tolak = 87374536427323676361 - 788578463743
+print(tolak)
+
+tambah = 100 + 1000 + 10000 + 10 + 1
+print(tambah)
+
+#variable - store values
+kucing = 99
+anjing = 1
+print(kucing)
+print(kucing + anjing)
+print(kucing + anjing + kucing)
+print(anjing + 70)
+
+#str() - integer to string
+umur = 30
+
+print("I am " + str(umur) + " years old")
+
+#int() - floating point to a whole integer
+perpuluhan = 9.5
+print(int(perpuluhan))          # 9
+print(int(perpuluhan) + 21)     # 30
+
+#practice questions
+bacon = 20
+print(bacon + 1)
+
+expression = 'spam' + "spamspam"
+print(expression)
+
+expression2 = 'spam' * 5
+print(expression2)
+
+# use str() to convert integer to string
+a = "I have eaten " + str(99) + " burritos"
+print(a)
