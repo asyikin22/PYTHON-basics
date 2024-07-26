900dfeaa6b333f69c46b3e7703721d295cdd8584
asyikin22 <148519441+asyikin22@users.noreply.github.com>
2024-07-26 18:39:59 +0800

    install pyperclip - error - resolved

--- AUTOMATE THE BORING STUFF/SIMPLE PROGRAM/3-strings.py
+++ AUTOMATE THE BORING STUFF/SIMPLE PROGRAM/3-strings.py
@@ -1,3 +1,12 @@
 #Multi-Clipboard Automatic Messages
 print('Multi-clipboard Automatic Messages')
 import pyperclip
+
+#copy text to the clipboard
+pyperclip.copy("Hello, Asyikin")
+
+#retrieve text from clipboard
+greeting = pyperclip.paste()
+
+#print the retrieved text
+print("Text from clipboard: ", greeting)
